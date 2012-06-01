10f55d25ddfdff38f4f6c69302ac241ba83ca043
Leonardo Vidarte <lvidarte@gmail.com>
2012-05-31 23:35:58 -0300

    Added users property in Document class.

--- client/lai/document.py
+++ client/lai/document.py
@@ -5,13 +5,29 @@ from data import Data
 
 class Document:
 
-    def __init__(self, data, id=None, tid=None, sid=None, synched=True, keys=None):
+    def __init__(self, data, id=None, tid=None,
+                 sid=None, synched=True, keys=None,
+                 users=None, usersdel=None):
         self.data = Data(data)
         self.id = id
         self.tid = tid
         self.sid = sid
         self.synched = synched
-        self.keys = keys
+        self.keys = keys or ''
+        self.users = users or []
+        self.usersdel = usersdel or []
+
+    def get_dict(self):
+        return {
+           'id'      : self.id,
+           'sid'     : self.sid,
+           'tid'     : self.tid,
+           'data'    : self.data,
+           'keys'    : self.keys,
+           'synched' : self.synched,
+           'users'   : self.users,
+           'usersdel': self.usersdel,
+        }
 
 
 if __name__ == '__main__':
