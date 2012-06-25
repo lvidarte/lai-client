d905c1f6fbf16375683b811fb300bf6f73781da5
Alfredo Ramirez <alfredormz@gmail.com>
2012-06-25 16:00:44 -0300

    Some changes to sqlite3 client
      * Change the driver to sqlite3 from the standard library
      * save method now returns the affected rows
      * implemented update from remotes updates
      * a lots of new tests

--- client/lai/db/sqlite.py
+++ client/lai/db/sqlite.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from pysqlite2 import dbapi2 as sqlite
+import sqlite3 as sqlite
 from lai.db.base import DBBase
 from lai import Document
 
@@ -10,10 +10,8 @@ class DBSqlite(DBBase):
         '''Connect to the database'''
 
         self.connection = sqlite.connect(self.config['NAME'])
-
+        self.connection.row_factory = sqlite.Row
         self.cursor = self.connection.cursor()
-        #self.cursor.execute("SELECT load_extension('%s')" % (self.config['REGEXP_EXTENSION_PATH']))
-        #self.cursor.execute("SELECT load_extension('/usr/lib/sqlite3/pcre.so')")
 
         if not self._table_exists():
             self._initialize_database()
@@ -32,6 +30,7 @@ class DBSqlite(DBBase):
 
     def _initialize_database(self):
         '''Create the table if doesn't exists'''
+
         try:
             self.cursor.execute('''CREATE TABLE %s
                                    (id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -48,43 +47,63 @@ class DBSqlite(DBBase):
             return False
         return True
 
-    def save(self, doc):
+    def save(self, doc, synched=False):
+
         if doc.id is None:
-            self._create(doc)
+            return self._create(doc, synched)
         else:
-            self._update(doc)
+            return self._update(doc, synched)
+
+    def update(self, doc, synched=False, pk='id'):
+
+        if pk == 'id':
+            self.save(doc, synched)
+        elif pk == 'sid':
+            id = self._exists('sid', doc.sid)
+            doc['id'] = id
+            self.save(doc, synched)
 
     def get(self, id):
-       self.cursor.execute('''SELECT * FROM %s
+
+        self.cursor.execute('''SELECT * FROM %s
                               WHERE id = %d''' % (self.config['TABLE'], id))
-       doc = Document()
-       row = self.cursor.fetchone()
+        row = self.cursor.fetchone()
+        if row is not None:
+            doc = Document(**row)
+            doc.users    = row[5].split(',')
+            doc.usersdel = row[6].split(',')
+        return doc
 
-       if row is not None:
-           doc.id       = row[0]
-           doc.sid      = row[1]
-           doc.tid      = row[2]
-           doc.data     = row[3]
-           doc.keys     = row[4]
-           doc.users    = row[5].split(',')
-           doc.usersdel = row[6].split(',')
-       return doc
+    def search(self, search_text):
 
-    def search(self, regex):
-        self.cursor.execute('''SELECT data
-                             FROM %s
-                             WHERE data LIKE '%s' ''' % (
-                                 self.config['TABLE'],
-                                 '%' + regex + '%'))
-        return self.cursor.fetchall()
+        docs = []
+        if (search_text is not None) and (not search_text == ''):
+            self.cursor.execute('''SELECT * FROM %s
+                                   WHERE data LIKE '%s' ''' % (self.config['TABLE'],
+                                                               '%' + search_text + '%'))
+            rows = self.cursor.fetchall()
+            for row in rows:
+                doc = Document(**row)
+                doc.users    = row[5].split(',')
+                doc.usersdel = row[6].split(',')
+                docs.append(doc)
+        return docs
 
     def get_docs_for_commit(self):
-        self.cursor.execute('''SELECT * FROM %s
-                               WHERE synched = 0''' % (self.config['TABLE']))
 
-        return self.cursor.fetchall()
+        docs = []
+        self.cursor.execute('''SELECT * FROM %s
+                               WHERE synched = 'False' ''' % (self.config['TABLE']))
+        rows = self.cursor.fetchall()
+        for row in rows:
+            doc = Document(**row)
+            doc.users    = row[5].split(',')
+            doc.usersdel = row[6].split(',')
+            docs.append(doc)
+        return docs
 
     def get_last_tid(self):
+
         self.cursor.execute('''SELECT tid
                                FROM %s
                                ORDER BY tid DESC
@@ -96,41 +115,58 @@ class DBSqlite(DBBase):
         else:
             return 0
 
-    def _create(self, doc):
+    def _create(self, doc, synched=False):
+
         sql_insert = '''INSERT INTO %s
            (tid, sid, data, keys, users, usersdel, synched)
-           VALUES ('%s','%s','%s','%s', '%s', '%s', %s)
+           VALUES ('%s','%s','%s','%s', '%s', '%s', '%s')
         '''
-        self.cursor.execute(sql_insert % (self.config['TABLE'],
+        rows_afected = self.cursor.execute(sql_insert % (self.config['TABLE'],
                                           doc.tid,
                                           doc.sid,
                                           doc.data,
                                           doc.keys,
-                                          ', '.join(doc.users),
-                                          ', '.join(doc.usersdel),
-                                          0))
+                                          ','.join(doc.users),
+                                          ','.join(doc.usersdel),
+                                          synched)).rowcount
         self.connection.commit()
+        return rows_afected
 
-    def _update(self, doc):
+    def _update(self, doc, synched=False):
+
         sql_update = '''UPDATE %s
                         SET sid      = '%s',
                             tid      = '%s',
                             data     = '%s',
                             keys     = '%s',
                             users    = '%s',
-                            usersdel = '%s'
+                            usersdel = '%s',
+                            synched  = '%s'
                         WHERE id = %d
                      '''
-
-        self.cursor.execute(sql_update % (self.config['TABLE'],
-                                          doc.sid, doc.tid, doc.data,
-                                          doc.keys,
-                                          ', '.join(doc.users),
-                                          ', '.join(doc.usersdel),
-                                          doc.id))
+        rows_afected = self.cursor.execute(sql_update % (self.config['TABLE'],
+                                                         doc.sid,
+                                                         doc.tid,
+                                                         doc.data,
+                                                         doc.keys,
+                                                         ','.join(doc.users),
+                                                         ','.join(doc.usersdel),
+                                                         synched,
+                                                         doc.id)).rowcount
         self.connection.commit()
+        return rows_afected
+
+    def _exists(self, key='id', value=None):
+
+        if value:
+            self.cursor.execute('''SELECT id FROM %s
+                                   WHERE %s = %s''' % (self.config['TABLE'], key, value))
+            return self.cursor.fetchone()[0]
+        else:
+            return False
 
     def docs_count(self):
+
         self.cursor.execute("SELECT COUNT(*) AS COUNT FROM %s" % self.config['TABLE'])
         return self.cursor.fetchone()[0]
 
