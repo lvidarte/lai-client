a6ff3f000c6ae034773523213edcf70df68c6ec9
Alfredo Ramirez <alfredormz@gmail.com>
2012-06-25 02:08:12 -0300

    Added support to sqlite3 in the client

--- client/lai/config.py
+++ client/lai/config.py
@@ -9,3 +9,11 @@ DATABASE = {
     'NAME'  : 'lai',
     'TABLE' : 'client1',
 }
+
+#DATABASE = {
+    #'ENGINE': 'sqlite',
+    #'NAME'  : 'lai.db',
+    #'TABLE' : 'client',
+    #'REGEXP_EXTENSION_PATH' : "/usr/lib/sqlite3/pcre.so",
+#}
+
--- client/tests/db/test_sqlite_database.py
+++ client/tests/db/test_sqlite_database.py
@@ -1,27 +1,59 @@
 import unittest
 import os
 from lai.db import DBSqlite
+from lai    import Document
 
-
-config = {'NAME': 'lai.db',
-          'TABLE': 'client1'}
 
 
 class TestDBSqlite(unittest.TestCase):
 
-    def setUp(self):
+    def _delete_dbfile(self):
         try:
-            os.remove(config['NAME'])
+            os.remove(self.config['NAME'])
         except OSError:
             pass
-        self.db = DBSqlite(config)
+
+    def setUp(self):
+        self.config = {'NAME': 'lai.db',
+                       'TABLE': 'client'}
+
+        self.db = DBSqlite(self.config)
+
+    def tearDown(self):
+        self._delete_dbfile()
 
     def test_connect_should_create_dbfile(self):
-        DBSqlite(config)
-        self.assertTrue(os.path.exists(config['NAME']))
+        DBSqlite(self.config)
+        self.assertTrue(os.path.exists(self.config['NAME']))
 
     def test_table_should_not_exists(self):
         self.assertFalse(self.db._table_exists('imposible_que_exista.db'))
 
     def test_table_creation(self):
+        self.db.config['TABLE'] = 'Probando' 
         self.assertTrue(self.db._initialize_database())
+
+    def test_docs_count(self):
+        self.assertEqual(self.db.docs_count(), 0)
+
+    def test_insert_new_document(self):
+        doc = Document(data='probando probando', keys='nada')
+        self.db.save(doc)
+        self.assertEqual(self.db.docs_count(), 1)
+
+    def test_get_doc_by_id(self):
+        new_doc = Document(sid=1, tid=2, data='sarlanga')
+        self.db.save(new_doc)
+        doc = self.db.get(1)
+        self.assertEqual(doc.data, 'sarlanga')
+
+    def test_update(self):
+        new_doc = Document(data='1 2 3')
+        self.db.save(new_doc)
+        edit_doc = self.db.get(1)
+        edit_doc.data = '1 2 3 4'
+        self.db.save(edit_doc)
+        
+        doc = self.db.get(1)
+        self.assertEqual(doc.data, '1 2 3 4')
+
