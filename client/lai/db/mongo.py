9fdba3b71ec6bc0bebd39136a264e0e859821f32
Leonardo Vidarte <lvidarte@gmail.com>
2012-06-30 11:58:04 -0300

    Use id autoincrement as pk in mongo driver

--- client/lai/db/mongo.py
+++ client/lai/db/mongo.py
@@ -5,11 +5,6 @@ from lai.db.base import DBBase
 from lai.database import UPDATE_RESPONSE, COMMIT_RESPONSE
 from lai import Document
 
-try:
-    from bson.objectid import ObjectId
-except ImportError:
-    from pymongo.objectid import ObjectId
-
 
 class DBMongo(DBBase):
 
@@ -25,21 +20,28 @@ class DBMongo(DBBase):
             return doc['tid']
         return 0
 
+    def get_next_id(self):
+        query = {'_id': self.config['TABLE']}
+        update = {'$inc': {'last_id': 1}}
+        collection = self.db['counter']
+        row = collection.find_and_modify(query, update, upsert=True, new=True)
+        return row['last_id']
+
     def search(self, regex):
-        cur = self.collection.find({'data': {'$regex': regex}})
+        spec = {'data': {'$regex': regex}}
+        fields = {'_id': 0}
+        cur = self.collection.find(spec, fields)
         docs = []
         for row in cur:
-            row['id'] = str(row['_id'])
-            del row['_id']
             docs.append(Document(**row))
         return docs
 
     def get(self, id):
-        rs = self.collection.find_one({'_id': ObjectId(id)})
-        if rs:
-            rs['id'] = str(rs['_id'])
-            del rs['_id']
-            doc = Document(**rs)
+        spec = {'id': int(id)}
+        fields = {'_id': 0}
+        row = self.collection.find_one(spec, fields)
+        if row:
+            doc = Document(**row)
             return doc
 
     def save(self, doc):
@@ -49,7 +51,8 @@ class DBMongo(DBBase):
             return self.insert(doc)
 
     def insert(self, doc):
-        doc_ = {'sid'     : doc.sid,
+        doc_ = {'id'      : self.get_next_id(),
+                'sid'     : doc.sid,
                 'tid'     : doc.tid,
                 'data'    : doc.data,
                 'keys'    : doc.keys,
@@ -57,13 +60,15 @@ class DBMongo(DBBase):
                 'usersdel': doc.usersdel,
                 'synched' : False}
         rs = self.collection.insert(doc_)
-        return str(rs)
+        if rs:
+            row = self.collection.find_one({'_id': rs})
+            return row['id']
+        return  False
 
     def update(self, doc, type=None):
         if type is None:
-            pk = '_id'
-            id = ObjectId(doc.id)
-            upsert = False
+            pk = 'id'
+            id = doc.id
             doc_ = {'sid'     : doc.sid,
                     'tid'     : doc.tid,
                     'data'    : doc.data,
@@ -72,9 +77,10 @@ class DBMongo(DBBase):
                     'usersdel': doc.usersdel,
                     'synched' : False}
         elif type == UPDATE_RESPONSE:
+            if self.collection.find({'sid': doc.sid}).count() == 0:
+                return self.insert(doc)
             pk = 'sid'
-            id = doc.id
-            upsert = True
+            id = doc.sid
             doc_ = {'sid'     : doc.sid,
                     'tid'     : doc.tid,
                     'data'    : doc.data,
@@ -83,34 +89,29 @@ class DBMongo(DBBase):
                     'usersdel': doc.usersdel,
                     'synched' : True}
         elif type == COMMIT_RESPONSE:
-            pk = '_id'
-            id = ObjectId(doc.id)
-            upsert = False
+            pk = 'id'
+            id = doc.id
             doc_ = {'sid'     : doc.sid,
                     'tid'     : doc.tid,
                     'synched' : True}
         else:
             return False
 
-        rs = self.collection.update({pk: id},
-                                    {'$set': doc_},
-                                    safe=True,
-                                    upsert=upsert)
+        rs = self.collection.update({pk: id}, {'$set': doc_}, safe=True)
         return rs['n'] == 1
 
     def delete(self, doc):
-        id = ObjectId(doc.id)
-        doc_ = {'data': None, 'keys': None}
-        rs = self.collection.update({'_id': id}, {'$set': doc_}, safe=True)
+        spec = {'id': doc.id}
+        document = {'$set': {'data': None, 'keys': None}}
+        rs = self.collection.update(spec, document, safe=True)
         return rs['n'] == 1
 
     def get_docs_for_commit(self):
-        rs = self.collection.find({'synched': False})
+        spec = {'synched': False}
+        fields = {'_id': 0, 'synched': 0}
+        rs = self.collection.find(spec, fields)
         docs = []
         for row in rs:
-            row['id'] = str(row['_id'])
-            del row['_id']
-            del row['synched']
             docs.append(row)
         return docs
 
