15648ba7fd3f5d70ea429ff387a9f994999e986f
Leonardo Vidarte <lvidarte@gmail.com>
2012-07-04 20:02:47 -0300

    Added some try-excepts in mongo driver.

--- client/lai/db/mongo.py
+++ client/lai/db/mongo.py
@@ -55,10 +55,14 @@ class DBMongo(DBBase):
     def get(self, id):
         spec = {'id': int(id)}
         fields = {'_id': 0}
-        row = self.collection.find_one(spec, fields)
+        try:
+            row = self.collection.find_one(spec, fields)
+        except Exception as e:
+            DatabaseException(e)
         if row:
             doc = Document(**row)
             return doc
+        return None
 
     def save(self, doc):
         if doc.id:
@@ -67,31 +71,20 @@ class DBMongo(DBBase):
             return self.insert(doc)
 
     def insert(self, doc, synched=False):
-        doc_ = {'id'      : self.get_next_id(),
-                'sid'     : doc.sid,
-                'tid'     : doc.tid,
-                'data'    : doc.data,
-                'keys'    : doc.keys,
-                'users'   : doc.users,
-                'usersdel': doc.usersdel,
-                'synched' : synched}
-        rs = self.collection.insert(doc_)
-        if rs:
-            row = self.collection.find_one({'_id': rs})
-            return row['id']
-        return  False
+        doc.id = self.get_next_id()
+        doc.synched = synched
+        try:
+            self.collection.insert(doc.to_dict())
+        except Exception as e:
+            raise DatabaseException(e)
+        return doc
 
     def update(self, doc, type=None):
         if type is None:
             pk = 'id'
             id = doc.id
-            doc_ = {'sid'     : doc.sid,
-                    'tid'     : doc.tid,
-                    'data'    : doc.data,
-                    'keys'    : doc.keys,
-                    'users'   : doc.users,
-                    'usersdel': doc.usersdel,
-                    'synched' : False}
+            doc.synched = False
+            doc_ = doc.to_dict()
         elif type == UPDATE_RESPONSE:
             if self.collection.find({'sid': doc.sid}).count() == 0:
                 return self.insert(doc, synched=True)
@@ -100,27 +93,25 @@ class DBMongo(DBBase):
                 doc.keys = None
             pk = 'sid'
             id = doc.sid
-            doc_ = {'sid'     : doc.sid,
-                    'tid'     : doc.tid,
-                    'data'    : doc.data,
-                    'keys'    : doc.keys,
-                    'users'   : doc.users,
-                    'usersdel': doc.usersdel,
-                    'synched' : True}
+            doc.synched = True
+            doc_ = doc.to_dict()
         elif type == COMMIT_RESPONSE:
             pk = 'id'
             id = doc.id
-            doc_ = {'sid'     : doc.sid,
-                    'tid'     : doc.tid,
-                    'synched' : True}
+            doc.synched = True
+            doc_ = {'sid': doc.sid, 'tid': doc.tid, 'synched': doc.synched}
             if config.USER in doc.usersdel:
-                doc_['data'] = None
-                doc_['keys'] = None
+                doc_.update(data=None, keys=None)
         else:
-            return False
+            raise DatabaseException('incorrect type')
 
-        rs = self.collection.update({pk: id}, {'$set': doc_}, safe=True)
-        return rs['n'] == 1
+        try:
+            rs = self.collection.update({pk: id}, {'$set': doc_}, safe=True)
+            assert rs['n'] == 1
+        except Exception as e:
+            raise DatabaseException(e)
+        else:
+            return doc
 
     def delete(self, doc):
         doc.data = None
@@ -128,13 +119,12 @@ class DBMongo(DBBase):
         return self.update(doc)
 
     def get_docs_for_commit(self):
-        spec = {'synched': False}
-        fields = {'_id': 0, 'synched': 0}
-        rs = self.collection.find(spec, fields)
-        docs = []
-        for row in rs:
-            docs.append(row)
-        return docs
+        try:
+            spec = {'synched': False}
+            fields = {'_id': 0, 'synched': 0}
+            return self.collection.find(spec, fields)
+        except Exception as e:
+            DatabaseException(e)
 
     def __str__(self):
         return "%s://%s:%s/%s?%s" % (self.config['ENGINE'],
