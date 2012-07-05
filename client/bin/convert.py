e8f328b7b6ce3a7c49abb301c86468dbc821725e
Leonardo Vidarte <lvidarte@gmail.com>
2012-07-05 00:08:27 -0300

    Added commit call in convert.py

--- client/bin/convert.py
+++ client/bin/convert.py
@@ -9,10 +9,13 @@ filename = '/home/xleo/src/tools12/trunk/scripts/lai/data'
 
 with codecs.open(filename, 'r', encoding='latin1') as file:
     lines = file.readlines()
+    count = 0
     for line in lines[1772:]:
         if line != '':
-            #data = line.rsplit('#')[0].strip()
             doc = Document(line.strip())
             doc.set_keys(line)
-            client.save(doc)
+            doc = client.save(doc)
+            if count % 25 == 0:
+                client.commit()
+            count += 1
 
