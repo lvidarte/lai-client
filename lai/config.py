8c3308d9e058ba1ee5a86d99809a503927187f3b
Leonardo Vidarte <lvidarte@gmail.com>
2012-07-29 23:03:00 -0300

    Updated .gitignore

--- lai/config.py
+++ lai/config.py
@@ -19,10 +19,11 @@ import os.path
 
 SERVER_ADDR = "lai.calcifer.com.ar"
 SERVER_PORT = 80
-#USER = "lvidarte@gmail.com"
-#KEY_NAME = "xleo@howl"
-#GITHUB_USER = "lvidarte"
-#GITHUB_PASSWORD = "********"
+USER = ""
+KEY_NAME = ""
+
+#GITHUB_USER = ""
+#GITHUB_PASSWORD = ""
 
 DATABASE1 = {
     'ENGINE': 'mongo',
@@ -43,3 +44,8 @@ DATABASE = DATABASE1
 SERVER_PUB_KEY = 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEApJD544PRvoA1O7Mu6A73NHhQ24qxh5W++yqEbEzU+hN/YqB9rFG99lG/qhuhgYqvxo8OLBMsO72WoEPteHHeZ0UIgxiN4JOhQhcLgH9fghP46RyUGNYuRCKUdaq7+6L3WGJWj6GbwNJudDgdnJJD9pFnf25cSjqGSP/kLT2BRvLO1ZC/tUZJwpLtuPi9akpuwmlTBBXlroVzJNVe7W6w+3BGmodqQN4R0fVnSf2Hg8ZQhsGKzf/JGySATneTU5g144H/Z6ALHmBM6XqY0Ql3Crz2xSCTrIbEsvvS5WNlaoo+QTOKWD8YNRJ44u+xHSZFF1rM3E1f3L7DdFptIzcl2w== lvidarte@web199.webfaction.com'
 
 CLIENT_PRV_KEY_PATH = os.path.join(os.path.expanduser('~'), ".ssh/id_rsa")
+
+try:
+    from local_config import *
+except:
+    pass
